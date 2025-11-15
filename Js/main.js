09532be59ae94ada0a1ec7a3141af7ba5aaead6e
Giza-2210 <mahizin255@gmail.com>
2025-11-15 04:55:48 -0600

    main completado, funcionalidad de los botones y opción de agregar funcionando adecuadamente

--- Js/main.js
+++ Js/main.js
@@ -208,9 +208,12 @@ const productos = [
 
 const contenedorProductos = document.querySelector("#contenedor-productos");
 const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelector(".producto-agregar");
+const numerito = document.querySelector(".numerito");
 function cargarProductos(productosElegidos) {
 
-    contenedorProductos.innerHTML = " ";
+   contenedorProductos.innerHTML = " ";
 
     productosElegidos.forEach(producto => {
 
@@ -227,7 +230,8 @@ function cargarProductos(productosElegidos) {
 
         contenedorProductos.append(div);
     })
-
+    
+    actualizarBotonesAgregar ();
 }
 
 cargarProductos(productos);
@@ -238,7 +242,47 @@ botonesCategorias.forEach(boton => {
         botonesCategorias.forEach(boton => boton.classList.remove("active"));
         e.currentTarget.classList.add("active");
 
-        const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
-        cargarProductos(productosBoton);
+        if(e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre
+
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos"
+            cargarProductos(productos)
+        }
+
     })
-})
+});
+
+function actualizarBotonesAgregar () {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+const productosEnCarrito = [];
+function agregarAlCarrito(e) {
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find (producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+       const index = productosEnCarrito.findIndex(producto => producto.id === idBoton );
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    } 
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
