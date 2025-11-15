531d67fd61af82a313c8dd1c89a82b2b59f2b514
Giza-2210 <mahizin255@gmail.com>
2025-11-15 08:13:19 -0600

    pagina casi completa, pendiente solo boton comprar

--- Js/carrito.js
+++ Js/carrito.js
@@ -127,7 +127,6 @@ function actualizarTotal() {
     const totalCalculado = productosEnCarrito.reduce((acc, producto) => acc + (producto.precio * producto.cantidad), 0);
     total.innerText = `$${totalCalculado}`;
 }
-
 botonComprar.addEventListener("click", comprarCarrito);
 function comprarCarrito() {
 
